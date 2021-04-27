e1a7d0cbae128595bb0bdc2103acf76229e292e1
Nidhirajput1301 <nidhirajput1301@gmail.com>
2021-04-28 01:08:06 +0530

    update &  delete form

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 from .forms import *
 
@@ -41,7 +41,6 @@ def customer(request, pk):
 def createOrder(request):
     form = OrderForm()
 
-
     if request.method == 'POST':
             #print('Printing POST:', request.POST)
             form = OrderForm(request.POST)
@@ -51,3 +50,28 @@ def createOrder(request):
 
     context = {'form': form}
     return render(request, 'accounts/order_form.html', context)
+
+
+def updateOrder(request, pk):
+
+	order = Order.objects.get(id=pk)
+	form = OrderForm(instance=order)
+
+	if request.method == 'POST':
+		form = OrderForm(request.POST, instance=order)
+		if form.is_valid():
+			form.save()
+			return redirect('/')
+
+	context = {'form': form}
+	return render(request, 'accounts/order_form.html', context)
+
+
+def deleteOrder(request, pk):
+	order = Order.objects.get(id=pk)
+	if request.method == "POST":
+		order.delete()
+		return redirect('/')
+
+	context = {'item': order}
+	return render(request, 'accounts/delete.html', context)
