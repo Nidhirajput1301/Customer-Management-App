3de0e3db30e9491549d061cda86e16b835f89351
Nidhirajput1301 <nidhirajput1301@gmail.com>
2021-04-28 22:46:57 +0530

    signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+
 from .models import *
 from .forms import *
 from .filters import OrderFilter
 
 # Create your views here.
+
+def registerPage(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+       form = CreateUserForm(request.POST)
+       if form.is_valid():
+         form.save()
+
+    context = {'form':form }
+    return render(request,'accounts/register.html', context)
+
+
+def loginPage(request):
+    context = {}
+    return render(request, 'accounts/login.html', context)
 
 
 def home(request):
