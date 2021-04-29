3ae553de9826f9af98418d01a4e1b88985c30e56
Nidhirajput1301 <nidhirajput1301@gmail.com>
2021-04-29 23:58:19 +0530

    user profile

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 class Customer(models.Model):
+	user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
 	name = models.CharField(max_length=200, null=True)
 	phone = models.CharField(max_length=200, null=True)
 	email = models.CharField(max_length=200, null=True)
+	profile_pic = models.ImageField(null=True, blank=True)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
 
   # for name of table object acc to us not like customer(1)
@@ -53,3 +56,7 @@ class Order(models.Model):
 
 	def __str__(self):
 		return(self.product.name)
+		# if self.product.name == None:
+		# 		return "ERROR-PRODUCT NAME IS NULL"
+		# return self.product.name
+		 
